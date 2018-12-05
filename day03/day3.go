3dd9ea068fb53d2d89a248815d4860290a46fb5f
Stefan Engstrom <sengstrom@ena.com>
2018-12-04 22:50:13 -0600

    day 3 cleaned up

--- day03/day3.go
+++ day03/day3.go
@@ -7,34 +7,35 @@ import (
 
 type claim struct {
 	id int
-	x int
-	y int
-	w int
-	h int
+	x  int
+	y  int
+	w  int
+	h  int
 }
 
-type coord struct {
+type coordinate struct {
 	x int
 	y int
 }
+
 func main() {
 	lines := aoc.Input("input.txt")
 	claims := parse(lines)
 
 	// Build the fabric
-	fabric := make(map[coord]int)
-	for _,c := range claims {
-		for i:=0;i<c.w;i++ {
-			for j:=0;j<c.h;j++ {
-				fabric[coord{c.x+i,c.y+j}]+=1
+	fabric := make(map[coordinate]int)
+	for _, c := range claims {
+		for i := 0; i < c.w; i++ {
+			for j := 0; j < c.h; j++ {
+				fabric[coordinate{c.x + i, c.y + j}] += 1
 			}
 		}
 	}
 
 	// Part 1 - count overlapping claims
 	var count int
-	for _,c := range fabric {
-		if c>=2 {
+	for _, c := range fabric {
+		if c >= 2 {
 			count++
 		}
 	}
@@ -42,17 +43,17 @@ func main() {
 	fmt.Printf("Part 1 - There are %d square inches with two or more claims\n", count)
 
 	// Part 2 - find the one non-overlapping claim
-	for _,c := range claims {
-		if ! isOverlapping(c, fabric) {
+	for _, c := range claims {
+		if !isOverlapping(c, fabric) {
 			fmt.Printf("Part 2 - This claim does not overlap: %d\n", c.id)
 		}
 	}
 }
 
-func isOverlapping(c *claim, fabric map[coord]int) bool {
-	for i:=0;i<c.w;i++ {
-		for j:=0;j<c.h;j++ {
-			if fabric[coord{c.x+i,c.y+j}]!=1 {
+func isOverlapping(c *claim, fabric map[coordinate]int) bool {
+	for i := 0; i < c.w; i++ {
+		for j := 0; j < c.h; j++ {
+			if fabric[coordinate{c.x + i, c.y + j}] != 1 {
 				return true
 			}
 		}
@@ -61,11 +62,14 @@ func isOverlapping(c *claim, fabric map[coord]int) bool {
 }
 
 func parse(lines []string) []*claim {
-	claims := make([]*claim,len(lines))
+	claims := make([]*claim, len(lines))
 	for k := range lines {
 		c := new(claim)
-		fmt.Sscanf(lines[k], "#%d @ %d,%d: %dx%d",&c.id, &c.x, &c.y, &c.w, &c.h)
-		claims[k]=c
+		_,err := fmt.Sscanf(lines[k], "#%d @ %d,%d: %dx%d", &c.id, &c.x, &c.y, &c.w, &c.h)
+		if err != nil {
+			panic("couldn't scan input")
+		}
+		claims[k] = c
 	}
 	return claims
 }
